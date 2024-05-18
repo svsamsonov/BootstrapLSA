d574e35dd01d60c657b601fef8971c49138247b8
svsamsonov <svsamsonov@w358992.staff.corp.local>
2024-05-18 14:48:56 +0300

    parallel fix

--- run_bootstrap_parallel.py
+++ run_bootstrap_parallel.py
@@ -29,7 +29,6 @@ def init_pi(N_a,N_s):
     #print(Pi_matr.sum(axis=0))
     return Pi_matr
 
-
 def generate_dynamics(N_a,N_s,b):
     """
     function to generate transition probabilities,
@@ -82,82 +81,12 @@ def init_rewards(N_a,N_s):
     R = Pi_matr = np.random.uniform(0.0,1.0,(N_a,N_s))
     return R
 
-#global constants
-#number of actions
-N_a = 2
-#number of states
-N_s = 10
-#gamma = 0.99
-gamma = 0.9
-#branching factor (external parameter for Garnet)
-branch = 3
-
-
-#init policy matrix
-Policy = init_pi(N_a,N_s)
-#init transition matrix
-P,Inds_nz = generate_dynamics(N_a,N_s,branch)
-#init rewards
-R = init_rewards(N_a,N_s)
-#init state transition matrix
-S_trans = state_transitions(P,Policy)
-
-#system matrix
-A = np.eye(N_s) - gamma*(S_trans.T)
-#right hand side
-b = np.sum(Policy*R,axis=0)
-theta_star = np.linalg.inv(A) @ b
-
-#note that in my notations they are usual (right) eigenvalues
-eigvals, eigfuncs = np.linalg.eig(S_trans)
-pi_states = -np.real(eigfuncs[:,0])
-pi_states = pi_states/np.sum(pi_states)
-
-
-#calculate covariance matrix Sigma_epsilon
-Sigma_eps = np.zeros((N_s,N_s),dtype=float)
-for s0 in range(N_s):
-    for s in range(N_s):
-        for a in range(N_a):
-            eps = np.zeros(N_s,dtype=float)
-            eps[s0] = R[a,s0] + gamma*theta_star[s]-theta_star[s0]
-            eps_upd = eps.reshape((-1,1)) @ eps.reshape((1,-1))
-            #averaging
-            Sigma_eps += pi_states[s0]*Policy[a,s0]*P[s,s0,a]*eps_upd
-
-A_star = np.zeros((N_s,N_s),dtype=float)
-for i in range(N_s):
-    for j in range(N_s):
-        A_star[i,j] = pi_states[i]*S_trans[j,i]
-A_star = np.diag(pi_states) - gamma*A_star
-b_star = b*pi_states
-theta_star_new = np.linalg.inv(A_star) @ b_star
-
-#Run TD(0) algorithm
-N_iters = 4*10**6
-v0 = 5*np.ones(N_s,dtype=float)
-s0 = np.random.choice(N_s)
-#step size
-c0 = 4.0
-alpha = np.zeros(N_iters,dtype=float)
-for i in range(N_iters):
-    alpha[i] = c0/np.sqrt(i+1)
-
-#compute cumulative sums
-Policy_cumulative = Policy.cumsum(axis=0)
-
-Cumulative_state = np.zeros((branch,N_s,N_a))
-for s in range(N_s):
-    for a in range(N_a):
-        cur_pol = P[Inds_nz[s,a],s,a]
-        Cumulative_state[:,s,a] = cur_pol.cumsum()
-
-def run_batch(num_estimates, N_max, n_total, alpha):
+def run_batch(Policy_cumulative,Cumulative_state,pi_states,Inds_nz,num_estimates, N_max, N_s, N_b, N_iters, n_total, alpha, R, gamma):
     V = 5*np.ones((N_max,N_s),dtype=float)
     ind_elem = 0
     V_cur = np.zeros_like(V)
     results = np.zeros((num_estimates,N_max,N_s),dtype=float)
-    for N in range(n_total):
+    for N in range(n_total+2):
         #generate a set of indices
         s0 = np.random.choice(N_s,  N_max, replace=True, p = pi_states)
         #sample action
@@ -175,84 +104,134 @@ def run_batch(num_estimates, N_max, n_total, alpha):
         #TD update
         V += alpha[N]*eps
         #update PR
-        if N == n0:
+        if N == N_b:
             V_cur = V
-        elif N > n0:
-            V_cur = (V_cur*(N-n0) + V) / (N-n0+1)
-        if N == n0 + N_iters[ind_elem]:
+        elif N > N_b:
+            V_cur = (V_cur*(N-N_b) + V) / (N-N_b+1)
+        if N == N_b + N_iters[ind_elem]:
             #fill averaged estimates one by one
             results[ind_elem,:,:] = V_cur
             ind_elem += 1
     return results
 
-def check_independent_last(seed,alpha,s_0,N_traj,n0,N_iters,num_workers=1):
+def check_independent_last(seed,Policy_cumulative,Cumulative_state,pi_states,Inds_nz,alpha,R,gamma,N_traj,N_b,N_iters,num_workers=1):
     N_max = 2048
     np.random.seed(seed)
-    #number of internediate estimates 
+    #number of intermediate estimates 
     num_estimates = len(N_iters)
     #total number of iterations: burn-in plus maximal number of iterations
     n_iters_max = np.max(N_iters)
-    n_total = n0 + n_iters_max
+    n_total = N_b + n_iters_max
     PR_V = np.zeros((num_estimates,N_traj,N_s),dtype=float)
     ###Main loop
     n_loops = N_traj // N_max
-    with ProcessPoolExecutor(max_workers=num_workers) as pool:
-        futures = []
-        for i in range(n_loops):
-            futures.append(pool.submit(run_batch, num_estimates, N_max, n_total, alpha))
-
-        for i, future in tqdm(enumerate(futures), total=len(futures)):
-            results = future.result()
-            PR_V[i*N_max:(i+1)*N_max,:] = results
+    print("n loops = ",n_loops)
+    if __name__ == '__main__':
+        with ProcessPoolExecutor(max_workers=num_workers) as pool:
+            futures = []
+            print("this should appear only once")
+            for i in range(n_loops):
+                print("submitted a job here number",i)
+                futures.append(pool.submit(run_batch, Policy_cumulative,Cumulative_state,pi_states,Inds_nz,num_estimates, N_max, N_s, N_b, N_iters, n_total, alpha,R, gamma))
+            print("all jobs submitted")
+            if __name__ == '__main__':
+                for i, future in tqdm(enumerate(futures), total=len(futures)):
+                    results = future.result()
+                    PR_V[:,i*N_max:(i+1)*N_max,:] = results
+                    print("i am locked here and just printing something")
     return PR_V
 
-#starting time
-start_time = time.time()
-#start loading parameters
-seed = 2024
-N_traj = 16384
-res_last = []
-res_pr = []
-#number of iterations
-N_b = 204800
-N_iters = [200,400,800,1600,3200,6400,12800,25600,51200,102400,204800,409600,819200,1638400]
-#N_iters = [1638400]
-N_iters = np.asarray(N_iters)
-pr_iter = check_independent_last(seed + i,alpha,s0,N_traj,N_b,N_iters[i], num_workers=16)
-#append strange arrays here
-#res_last.append(last_iter)
-res_pr.append(pr_iter)
-np.save(f"result/PR_V_iter={i},seed={seed+i},alpha={alpha},N_traj={N_traj},N_iters={N_iters[i]}.npy", pr_iter)
+"""
+Here the executable part of the script begins
+"""
 
-np.random.seed(2024)
-#generate random direction
-U = np.random.randn(N_s)
-u = U/np.linalg.norm(U)
-#compute the emirical CDF 
-pr_samples = []
+if __name__ == '__main__':
+    #global constants
+    #number of actions
+    N_a = 2
+    #number of states
+    N_s = 10
+    #gamma = 0.99
+    gamma = 0.9
+    #branching factor (external parameter for Garnet)
+    branch = 3
 
-for i in range(len(N_iters)):
-    norm_samples = np.sqrt(N_iters[i]//2)*A_star @ (res_pr[i] - theta_star.reshape((1,-1))).transpose()
-    pr_samples.append(norm_samples)
+    #init policy matrix
+    Policy = init_pi(N_a,N_s)
+    #init transition matrix
+    P,Inds_nz = generate_dynamics(N_a,N_s,branch)
+    #init rewards
+    R = init_rewards(N_a,N_s)
+    #init state transition matrix
+    S_trans = state_transitions(P,Policy)
 
-sample = []
-for i in range(len(N_iters)):
-    err_proj = pr_samples[i].transpose() @ u
-    sample.append(err_proj)
-sample = np.asarray(sample)
+    #system matrix
+    A = np.eye(N_s) - gamma*(S_trans.T)
+    #right hand side
+    b = np.sum(Policy*R,axis=0)
+    theta_star = np.linalg.inv(A) @ b
 
-sample_pickl = {'description':"Experiments 07.04.2024, step size k^{-1/2}, 819200 trajectories",'samle size':N_iters,'sample':sample}
-with open('res_07_04.pickle', 'wb') as handle:
-    pickle.dump(sample_pickl, handle)
+    #note that in my notations they are usual (right) eigenvalues
+    eigvals, eigfuncs = np.linalg.eig(S_trans)
+    pi_states = -np.real(eigfuncs[:,0])
+    pi_states = pi_states/np.sum(pi_states)
 
-all_ks = []
-loc = 0.0
-scale = (Sigma_eps @ u).dot(u)
-for i in range(len(N_iters)):
-    sample_sort = np.sort(sample[i])
-    ecdf = np.linspace(0,1,N_traj+1)
-    cdfs = norm.cdf(sample_sort, loc = 0, scale = np.sqrt(scale))
-    ks_dist = max([(ecdf[1:] - cdfs).max(), (cdfs - ecdf[:-1]).max()])
-    all_ks.append(ks_dist)
 
-print("total time elapsed: ",time.time()-start_time)
+    #calculate covariance matrix Sigma_epsilon
+    Sigma_eps = np.zeros((N_s,N_s),dtype=float)
+    for s0 in range(N_s):
+        for s in range(N_s):
+            for a in range(N_a):
+                eps = np.zeros(N_s,dtype=float)
+                eps[s0] = R[a,s0] + gamma*theta_star[s]-theta_star[s0]
+                eps_upd = eps.reshape((-1,1)) @ eps.reshape((1,-1))
+                #averaging
+                Sigma_eps += pi_states[s0]*Policy[a,s0]*P[s,s0,a]*eps_upd
+    print(Sigma_eps)
+    
+    A_star = np.zeros((N_s,N_s),dtype=float)
+    for i in range(N_s):
+        for j in range(N_s):
+            A_star[i,j] = pi_states[i]*S_trans[j,i]
+    A_star = np.diag(pi_states) - gamma*A_star
+    b_star = b*pi_states
+    theta_star_new = np.linalg.inv(A_star) @ b_star
+
+    #Run TD(0) algorithm
+    N_max_steps = 4*10**6
+    v0 = 5*np.ones(N_s,dtype=float)
+    s0 = np.random.choice(N_s)
+    #step size
+    c0 = 4.0
+    alpha = np.zeros(N_max_steps,dtype=float)
+    for i in range(N_max_steps):
+        alpha[i] = c0/np.sqrt(i+1)
+
+    #compute cumulative sums
+    Policy_cumulative = Policy.cumsum(axis=0)
+
+    Cumulative_state = np.zeros((branch,N_s,N_a))
+    for s in range(N_s):
+        for a in range(N_a):
+            cur_pol = P[Inds_nz[s,a],s,a]
+            Cumulative_state[:,s,a] = cur_pol.cumsum()
+            
+    #starting time
+    start_time = time.time()
+    #start loading parameters
+    seed = 2024
+    N_traj = 163840
+    res_last = []
+    res_pr = []
+    #number of iterations
+    N_b = 204800
+    N_iters = [200,400,800,1600,3200,6400,12800,25600,51200,102400]#,204800,409600,819200,1638400]
+    #N_iters = [1638400]
+    N_iters = np.asarray(N_iters)
+    pr_iter = check_independent_last(seed,Policy_cumulative,Cumulative_state,pi_states,Inds_nz,alpha,R,gamma,N_traj,N_b,N_iters,num_workers=16)
+    #append strange arrays here
+    #res_last.append(last_iter)
+    res_pr.append(pr_iter)
+    np.save(f"result/PR_V_iter={N_b+N_iters[-1]},seed={seed},alpha={0.5},N_traj={N_traj},N_iters={N_iters[-1]}.npy", pr_iter)
+
+    print("total time elapsed: ",time.time()-start_time)
